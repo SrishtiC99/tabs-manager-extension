398ceaffb53f05976608c9758921377b5e83b170
SrishtiC99 <iamsri9631@gmail.com>
2023-12-19 17:43:59 +0530

    enhanced css, toogle button to show tab urls

--- popup.js
+++ popup.js
@@ -1,36 +1,53 @@
+// all active tabs
 const tabs = await chrome.tabs.query({
     url: [
         "https://*/*",
     ]
 });
 
+// Sort Tab list by their title
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
 
+//load intial tab list with no URL
 const template = document.getElementById("li_template");
 const elements = new Set();
+loadTabList(false);
 
-for (const tab of tabs) {
-    const element = template.content.firstElementChild.cloneNode(true);
+// load tab list when toggle switch is clicked
+var toggleSwitch = document.getElementById("toggle_switch");
+toggleSwitch.addEventListener("click", () => {
+    loadTabList(toggleSwitch.checked)
+});
 
-    const title = tab.title.trim();
-    const pathname = new URL(tab.url).pathname.trim();
+function loadTabList(isChecked) {
+    elements.clear();
+    console.log(isChecked);
+    for (const tab of tabs) {
+        const element = template.content.firstElementChild.cloneNode(true);
 
-    console.log(title, pathname);
+        element.querySelector(".title").textContent = tab.title.trim();
+        const urlElement = element.querySelector(".url");
+        if (isChecked) {
+            urlElement.style.display = "block";
+            urlElement.textContent = tab.url;
+        }
+        else {
+            urlElement.style.display = "none";
+        }
+        // Made this tab active when the list item is clicked
+        element.querySelector("a").addEventListener("click", async () => {
+            await chrome.tabs.update(tab.id, { active: true });
+            await chrome.windows.update(tab.windowId, { focused: true });
+        });
 
-    element.querySelector(".title").textContent = title;
-    element.querySelector(".pathname").textContent = pathname;
-
-    element.querySelector("a").addEventListener("click", async () => {
-        await chrome.tabs.update(tab.id, { active: true });
-        await chrome.windows.update(tab.windowId, { focused: true });
-    });
-
-    elements.add(element);
+        elements.add(element);
+    }
+    document.querySelector("ul").innerHTML = "";
+    document.querySelector("ul").append(...elements);
 }
 
-document.querySelector("ul").append(...elements);
-
+// Create tab groups based on unique domains
 const groupTemplate = document.getElementById("gr_template");
 const groupElements = new Set();
 
@@ -49,14 +66,14 @@ for (const mainDomain of uniqueMainDomains) {
 
 document.querySelector("ol").append(...groupElements);
 
+// Group tab when button is clicked
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-    // Create tab groups based on unique domains
     for (const mainDomain of uniqueMainDomains) {
         const tabIds = tabs.filter(tab => getMainDomain(tab.url)  === mainDomain).map(tab => tab.id);
         const group = await chrome.tabs.group({ tabIds });
 
-        await chrome.tabGroups.update(group, { title: mainDomain });
+        await chrome.tabGroups.update(group, { title: mainDomain, collapsed: true });
     }
 })
 
@@ -66,7 +83,7 @@ async function groupThisTab(domain) {
     const tabIds = tabs.filter(tab => getMainDomain(tab.url) === domain).map(tab => tab.id);
     const group = await chrome.tabs.group({ tabIds });
 
-    await chrome.tabGroups.update(group, { title: domain });
+    await chrome.tabGroups.update(group, { title: domain, collapsed: true });
 }
 
 
